b4dd4fa44e28e38d89968196298133385a3da457
GharehzadehAli <gharehzadehali@gmail.com>
2019-06-06 00:46:34 +0430

    first part has been done

---
+++ Refactoring/src/main/java/com/example/Cat.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Cat extends Animal {
+    String breed;
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+}
