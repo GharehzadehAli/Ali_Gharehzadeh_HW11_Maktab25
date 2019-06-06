6fe81c108de76e3c4df2f30cd92ccbf40b1be5fe
GharehzadehAli <gharehzadehali@gmail.com>
2019-06-06 16:14:29 +0430

    forth part has been done

--- Refactoring/src/test/java/com/example/AppTest.java
+++ Refactoring/src/test/java/com/example/AppTest.java
@@ -1,20 +1,52 @@
 package com.example;
 
+import static com.example.App.*;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
+
+    @Test
+    public void createCatsTester() {
+        Assert.assertNotNull(createCats());
+    }
+
+    @Test
+    public void setCatNamesTester() {
+        List<Cat> list = createCats();
+        setCatNames(list);
+        for (int i = 0; i < 98; i++) {
+            Assert.assertEquals("cat" + i, list.get(i).getName());
+        }
+    }
+    @Test
+    public void setCatBreedsTester() {
+        List<Cat> list = createCats();
+        setCatBreeds(list);
+        List<BreedType> breedTypes= Arrays.asList(BreedType.values());
+        for (int i = 0; i < 98; i++) {
+            Assert.assertEquals(breedTypes.get(i), list.get(i).getBreed());
+        }
+    }
+    @Test
+    public void generateNewBreedTypeListTester(){
+
+    }
+
 }
